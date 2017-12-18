81b66924e63a54fae89ca01468c764b3b94db2a8
atalmondblossom <souiss318@gmail.com>
2017-12-18 15:48:36 +0900

    second commit

--- run.c
+++ run.c
@@ -4,6 +4,9 @@
 #include "run.h"
 #include "util.h"
 
+p_meta head = 0;
+p_meta last = 0;
+
 void *base = 0;
 
 p_meta find_meta(p_meta *last, size_t size) {
@@ -13,7 +16,17 @@ p_meta find_meta(p_meta *last, size_t size) {
   switch(fit_flag){
     case FIRST_FIT:
     {
-      //FIRST FIT CODE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	if(*last==0) return 0;
+
+	index=(*last)->prev;
+	while(index) {
+		if(index->free==0 || index->size < size)
+			index=index->prev;
+		else{	
+			result=index;
+			break;
+		}
+	}		
     }
     break;
 
@@ -34,6 +47,38 @@ p_meta find_meta(p_meta *last, size_t size) {
 }
 
 void *m_malloc(size_t size) {
+	p_meta new=base;
+	p_meta prev=base;
+	p_meta next=base;
+	p_meta temp=base;
+
+	if(new = find_meta(&last, size)){ 
+    	if(new->size > size + META_SIZE){ 
+      next = new->next;               
+      temp->next = next;
+      temp->prev = new;
+      temp->size = new->size - META_SIZE; 
+      temp->free = 1;                     
+      new->next = temp;
+      next->prev = temp;
+    }
+    new->free = 0;
+    return new + META_SIZE;
+  }
+  else{                             
+    new = sbrk(META_SIZE);
+    sbrk(size);
+    temp = last->prev;
+    last = sbrk(0);
+    new->prev = temp;
+    new->next = last;
+    temp->next = new;
+    last->prev = new;
+    new->size = size;
+    new->free = 0;
+    return new + META_SIZE;
+}
+
 
 }
 
@@ -41,8 +86,7 @@ void m_free(void *ptr) {
 
 }
 
-void*
-m_realloc(void* ptr, size_t size)
+void* m_realloc(void* ptr, size_t size)
 {
 
 }
